82852025d63f06fa2d16559ca9e662d4487cb5d8
Addison Huschka <yg0531qj@go.minnstate.edu>
2026-01-11 20:51:31 -0600

    practice makes perfect

---
+++ Week 1/M.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Sum: " + (a + b));
+
+    }
+}
