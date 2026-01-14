f70b5e6a83ea976e47073d04a5da62a71258d9f1
Addison Huschka <yg0531qj@go.minnstate.edu>
2026-01-14 13:02:19 -0600

    Update M.java

--- Week 1/M.java
+++ Week 1/M.java
@@ -1,10 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Sum: " + (a + b));
+        int user_temp = 
+        int fahrenheittemp = (user_temp * 9/5 + 32);
+        System.out.println(fahrenheittemp);
 
     }
 }
