e206afdcb46ed84c88769428844469db01366c48
Addison Huschka <yg0531qj@go.minnstate.edu>
2026-01-14 14:13:14 -0600

    Update M.java

--- Week 1/M.java
+++ Week 1/M.java
@@ -3,8 +3,8 @@ public static void main(String[] args) {
     
 System.out.println("Hi");
 int x = 7;
-while (x>4) {
-    System.out.println(x+4);
+while (x>0) {
+    System.out.println((x+4)*3);
     x=x-1;
 }
 
