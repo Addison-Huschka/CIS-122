8175508ca9cce62e2053630cb7dde280b47f7705
Addison Huschka <yg0531qj@go.minnstate.edu>
2026-01-25 18:48:23 -0600

    Update ClassesandObjects.java

--- Week 2/ClassesandObjects.java
+++ Week 2/ClassesandObjects.java
@@ -1,3 +1,42 @@
-public class ClassesandObjects {
-    
+import java.util.Scanner;
+
+class OddSum
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      int a = in.nextInt();
+      int b = in.nextInt();
+      int total = 0;
+      while (a <= b);{
+         if (a % 2 == 1);{
+             total = total + a;
+         }
+         if (a % 2 == -1);{
+            total = total + a;
+         }
+         a = a + 1;}
+      System.out.println(total);
+   }
+}
+
+/**
+   Counts the number of digits with value 7 in the decimal 
+   representation of the integer n
+*/
+class CountSevens
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      int count = 0;
+      while (n>0);{
+         if (n % 10 == 7);{
+            count = count + 1;
+         }
+         n /= 10;
+      }
+      System.out.println(count);
+   }
 }
